8b05a40d07dae58fc09f96d77ff89053b7d13026
NatalieGallegos <ngallegos.ng@gmail.com>
2016-11-13 18:57:52 -0800

    Finished users home page. Paging added to each list, and each list can drop down to organize page a bit more.

---
+++ movievultures/src/main/webapp/res/js/userHomePaging.js
@@ -0,0 +1,92 @@
+var current_page_rev = 1;
+var current_page_favs = 1;
+var current_page_watch = 1;
+
+var records_per_page = 5;
+
+function changeRevBy(num, tableName) {
+	// console.log('#' + tableName + " tr");
+	if (current_page_rev + num >= 1
+			&& current_page_rev + num <= numPages('#' + tableName + ' tr')) {
+		current_page_rev += num;
+		changePage(current_page_rev, tableName);
+	}
+}
+
+function changeWatchBy(num, tableName) {
+	if (current_page_watch + num >= 1
+			&& current_page_watch + num <= numPages('#' + tableName + ' tr')) {
+		current_page_watch += num;
+		changePage(current_page_watch, tableName);
+	}
+}
+
+function changeFavBy(num, tableName) {
+	if (current_page_favs + num >= 1
+			&& current_page_favs + num <= numPages('#' + tableName + ' tr')) {
+		current_page_favs += num;
+		changePage(current_page_favs, tableName);
+	}
+}
+
+function changePage(page, tableName) {
+	// Validate page
+	if (page < 1)
+		page = 1;
+	if (page > numPages('#' + tableName + ' tr'))
+		page = numPages('#' + tableName + ' tr');
+
+	$("." + tableName).hide();
+	$("." + tableName + "page" + page).show();
+	$("#" + tableName + "pageno").html(page);
+	for (var i = 1; i <= 4; i++) {
+		if (page - i >= 1) {
+			$("#" + tableName + "pageno-" + i).show().html(page - i);
+
+		} else {
+			$("#" + tableName + "pageno-" + i).hide();
+		}
+		// console.log($("#userpageno" + i));
+		if (page + i <= numPages("#" + tableName + " tr")) {
+			$("#" + tableName + "pageno" + i).show().html(page + i);
+		} else {
+			$("#" + tableName + "pageno" + i).hide();
+		}
+	}
+
+	if (page == 1) {
+		$("#" + tableName + "_btn_prev").hide();
+	} else {
+		$("#" + tableName + "_btn_prev").show();
+	}
+
+	if (page == numPages("#" + tableName + " tr")) {
+		$("#" + tableName + "_btn_next").hide();
+	} else {
+		$("#" + tableName + "_btn_next").show();
+	}
+
+}
+
+function numPages(tableName) {
+	var count = $(tableName).length;
+	return Math.ceil(count / records_per_page);
+}
+
+function toggleFavorites() {
+	$('#favSlider').slideToggle();
+};
+
+function toggleWatch() {
+	$('#watchSlider').slideToggle();
+}
+
+function toggleReviews() {
+	$('#revSlider').slideToggle();
+}
+
+window.onload = function() {
+	changePage(1, "rev");
+	changePage(1, "watch");
+	changePage(1, "fav");
+};
