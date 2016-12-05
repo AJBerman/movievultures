2772f5016bf8dbee567e285d953ed85f5c59a2b9
NatalieGallegos <ngallegos.ng@gmail.com>
2016-12-04 21:43:27 -0800

    Added autocomplete for user search

--- movievultures/src/main/java/movievultures/web/service/UserService.java
+++ movievultures/src/main/java/movievultures/web/service/UserService.java
@@ -1,5 +1,7 @@
 package movievultures.web.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -60,6 +62,13 @@ public class UserService {
     		user.getWatchLater().remove(index);
     	//save user
     	userDao.saveUser(user);
+    }
+    
+    //Service to return user names or list of strings
+    @RequestMapping(value = "/service/users/{term}", method=RequestMethod.GET)
+    @ResponseBody
+    public List<String> getUserNames(@PathVariable String term){
+    	return userDao.getUsernames(term);
     }
 
 }
